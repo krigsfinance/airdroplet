55aafa0cf167d610917e1071cfa9197b91152e8c
Samuel JJ Gosling <samuel.jj.gosling@gmail.com>
2019-01-07 20:09:06 +0100

    Update Airdroplet.sol

--- contracts/Airdroplet.sol
+++ contracts/Airdroplet.sol
@@ -1,27 +1,34 @@
 pragma solidity ^0.4.24;
 
-import "./ERC20.sol";
+interface ERC20 {
 
-contract Airdroplet 
-{
+  function totalSupply() external view returns (uint256);
+
+  function balanceOf(address who) external view returns (uint256);
+
+  function transfer(address to, uint256 value) external returns (bool);
+
+  function allowance(address owner, address spender) external view returns (uint256);
+
+  function transferFrom(address from, address to, uint256 value) external returns (bool);
+
+  function approve(address spender, uint256 value) external returns (bool);
+
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+}
+
+contract Airdroplet {
 
     ERC20 public token;
 
-    function airdropExecute(address source, address[] recipents, uint256 amount) public
-    {
-
-        uint x = 0;
+    function airdropExecute(address source, address[] recipents, uint256 amount) public {
         token = ERC20(source);
-
-        while(x < recipents.length)
-        {
-
-          token.transferFrom(msg.sender, recipents[x], amount);
-          x++;
-
+        for(uint x = 0; x < recipents.length; x++) {
+            token.transferFrom(msg.sender, recipents[x], amount);
         }
-
     }
-
 
 }
