bf7333fd1953555ba8d2a4fe01bd08a64c70a15b
samgos <samuel.jj.gosling@gmail.com>
2019-06-28 02:01:33 +0200

    reform dAp initialisation

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var Airdroplet = artifacts.require("./Airdroplet.sol");
-var Token = artifacts.require("./Token.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Token);
-  deployer.deploy(Airdroplet);
-
-};
